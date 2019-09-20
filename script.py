e1831707358e698d1a07a7a760c534d24fccacfe
Deborah Tampubolon <dajtpbl@gmail.com>
2019-09-20 12:12:16 +0700

    Initialize script.py

---
+++ script.py
@@ -0,0 +1,24 @@
+"""
+Deskripsi script
+"""
+
+def mini_batch(X, y) :
+    result = []
+    return result
+
+def node_output(inputs, weights, biases) :
+    return 0
+
+def activation_function(output) :
+    return 0
+
+def count_error(output, desired_output) :
+    return 0
+
+def train() :
+    return 0
+
+def predict():
+    return []
+    
+# Main program
