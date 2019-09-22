6a29a6719909eb324f759e32164809921af5f541
Muhammad Alif Arifin <m.alif.arifin@gmail.com>
2019-09-22 20:39:32 +0700

    update class

--- MiniBatch.py
+++ MiniBatch.py
@@ -1,3 +1,5 @@
+import random
+
 class MiniBatch:
     # data structure
     # weights
@@ -10,13 +12,40 @@ class MiniBatch:
         self.momentum = momentum
         self.epoch = epoch
 
+    def __random_weights(self) :
+        self.n_features = len(self.X_train.columns)
+
+        self.weights = []
+
+        weight = []
+        for i in range (0, self.n_features) :
+            weight.append(random.random())
+        self.weights.append(weight)
+
+        for i in range (0, self.hidden_layer) :
+            weight = []
+            for j in range (0, self.nb_nodes) :
+                weight.append(random.random())
+            self.weights.append(weight)
+
+    def __forward_pass(self) :
+
+    def __backward_pass(self) :
+
+    def __update(self) :
+        
+    def __sigmoid(self) :
+
     def fit(self, X, y) :
         # X is pandas.dataframe
         # y is pandas.series
+        self.__X_train = X
+        self.__y_train = y
+        self.__random_weights()
 
         return
 
     def predict(self, X) :
         # X is pandas.dataframe
-        
+
         return
