74f16edbf1b1062e610d0fb965dd58318af8840c
Muhammad Alif Arifin <m.alif.arifin@gmail.com>
2019-09-27 21:25:50 +0700

    add weight before

--- MiniBatch.py
+++ MiniBatch.py
@@ -8,6 +8,7 @@ class MiniBatch:
     __errors = []
     __outputs = []
     __weights = []
+    __weights_bef = []
 
     def __init__(self, nb_nodes, hidden_layer, batch_size, learning_rate, momentum, epoch) :
         self.__nb_nodes = nb_nodes
@@ -22,26 +23,31 @@ class MiniBatch:
         self.__n_features = len(self.__X_train.columns)
 
         self.__weights = []
+        self.__weights_bef = []
 
         # number of nodes for each layer
         self.__n_nodes = [self.__n_features] + [self.__nb_nodes] * self.__hidden_layer + [1] # 1 for output
 
         for i in range (0, self.__hidden_layer - 1) :
             b = []
-            for j in range (0, self.__n_nodes[i] + 1) :
+            b_bef = []
+            for _ in range (0, self.__n_nodes[i] + 1) :
                 # index 0 for bias
                 c = []
-                for k in range (0, self.__n_nodes[i + 1]) :
+                c_bef = []
+                for _ in range (0, self.__n_nodes[i + 1]) :
                     c.append(random.random())
+                    c_bef.append(0)
                 b.append(c)
+                b_bef.append(c_bef)
             self.__weights.append(b)
-            
+            self.__weights_bef.append(b_bef)
         
     def __sigmoid(self, v) :
         return 1/(1 + math.exp(-v))
 
     def __psi_apostrophe(self, value) :
-        return self.__sigmoid(value) * (1 - self.__sigmoid(value))
+        return value * (1 - value)
         
     def __generate_batch(self) :
         return 
@@ -101,6 +107,13 @@ class MiniBatch:
         self.__X_train = X
         self.__y_train = y
         self.__random_weights()
+        batches = self.__generate_batch()
+
+        for batch in batches :
+            self.__batch = batch
+            self.__forward_pass()
+            self.__backward_pass()
+            self.__update_weights()
 
         return
 
